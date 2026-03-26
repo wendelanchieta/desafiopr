6c9da65567625888d4bc631d55e6d905d0440633
Wendel Anchieta <wendelanchieta@gmail.com>
2026-03-26 11:33:17 -0300

    correção de imports nos arquivos de testes.

--- backend/orders/app/main.py
+++ backend/orders/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import orders
+from .routers import orders
 from app.database import engine, Base
 
 
--- backend/users/app/main.py
+++ backend/users/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth
+from .routers import auth
 from app.database import engine, Base
 
 app = FastAPI(title="Serviço de Usuários - Auth")
